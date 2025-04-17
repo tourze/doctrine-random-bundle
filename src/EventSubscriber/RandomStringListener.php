418d7141700090aa11bd3dcf220542d668ab2b10
github-ci <you@example.com>
2025-04-17 10:50:30 +0000

    CI: Fix errors

--- src/EventSubscriber/RandomStringListener.php
+++ src/EventSubscriber/RandomStringListener.php
@@ -27,7 +27,7 @@ class RandomStringListener implements EntityCheckerInterface
     private function generateRandomString(int $length = 10): string
     {
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = mb_strlen($characters);
+        $charactersLength = strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; ++$i) {
             $randomString .= $characters[random_int(0, $charactersLength - 1)];
