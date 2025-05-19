901df359e5d0ee20bdcea634b1129be74145d022
github-ci <you@example.com>
2025-05-19 15:43:05 +0000

    chore: 注册 PropertyAccessor 服务并使用属性注入

--- src/EventSubscriber/RandomStringListener.php
+++ src/EventSubscriber/RandomStringListener.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\ObjectManager;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\PropertyAccess\PropertyAccessor;
 use Tourze\DoctrineEntityCheckerBundle\Checker\EntityCheckerInterface;
 use Tourze\DoctrineRandomBundle\Attribute\RandomStringColumn;
@@ -19,7 +20,7 @@ use Tourze\DoctrineRandomBundle\Attribute\RandomStringColumn;
 class RandomStringListener implements EntityCheckerInterface
 {
     public function __construct(
-        private readonly PropertyAccessor $propertyAccessor,
+        #[Autowire(service: 'doctrine-random.property-accessor')] private readonly PropertyAccessor $propertyAccessor,
         private readonly LoggerInterface $logger,
     ) {
     }
